6730d1ef0fd6ff9f8779969a37e740261c2df689
Gabriel Trindade <gabriel.trindade@estudante.fism.edu.br>
2024-02-21 16:55:11 -0400

    Aula do dia 21/02

---
+++ DBF/database/seeders/AlunosTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class AlunosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('alunos')->insert([
+            ['nome'=>'Gabriel Ragale', 'email'=>'gabriel.trin@estudante.ifms.edu.br', 'data_nascimento' =>'2007-01-01','cursso'=>'Desenvolvimento baseado em frameworks']
+        ]);
+        }
+}
